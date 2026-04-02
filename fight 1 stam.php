9b93fe9d7c13acd80d4f585f47f651604fafe6fb
Nullb0dy404 <95555599+nobody65321@users.noreply.github.com>
2026-04-02 15:01:32 -0500

    Create fight 1 stam.php

---
+++ fight 1 stam.php
@@ -0,0 +1,96 @@
+Request URL
+https://demonicscans.org/damage.php
+Request Method
+POST
+Status Code
+200 OK
+Remote Address
+[2606:4700:3030::ac43:9395]:443
+Referrer Policy
+strict-origin-when-cross-origin
+alt-svc
+h3=":443"; ma=86400
+cache-control
+no-store, no-cache, must-revalidate, max-age=0
+cache-control
+max-age=172800
+cf-cache-status
+DYNAMIC
+cf-ray
+9e626b39cbf5532e-ATL
+content-encoding
+zstd
+content-type
+application/json; charset=utf-8
+date
+Thu, 02 Apr 2026 20:00:17 GMT
+expires
+Sat, 04 Apr 2026 20:00:17 GMT
+nel
+{"report_to":"cf-nel","success_fraction":0.0,"max_age":604800}
+pragma
+no-cache
+priority
+u=1,i
+report-to
+{"group":"cf-nel","max_age":604800,"endpoints":[{"url":"https://a.nel.cloudflare.com/report/v4?s=dE2j7TPcFEmzn7hcrEDh5uN7Fb30hcWAK0%2FEbJxRWtwriEnvOppw5EeoDJAfMvr8y90vbUHMVhaozpvpwXRx4LwUQo7sat1hu7NbyBeV5%2Btz16KuEydDxgS34lb%2FuzbBMKaDWF7sajMcgj8VNTWH"}]}
+server
+cloudflare
+server-timing
+cfExtPri
+x-debug-id
+bd72d757
+x-powered-by
+PHP/7.4.19
+:authority
+demonicscans.org
+:method
+POST
+:path
+/damage.php
+:scheme
+https
+accept
+*/*
+accept-encoding
+gzip, deflate, br, zstd
+accept-language
+en-US,en;q=0.9
+content-length
+56
+content-type
+application/x-www-form-urlencoded
+cookie
+_ga=GA1.1.970959827.1771607793; cf_clearance=UmZ4VDSth3tzr3TkcwKhVkmpVNemudDOExks9VLxZ2U-1772561977-1.2.1.1-R3nvhlUe.wBN6yeMU5xqLhjCfTM4poCcMdK96PeU2dxpwzAYYkDFZZtcyw5oihm4eOlqn8tbM.UMvKQaI6JVL20uSrYGncmdWQRyBt.sYChYYPUfIShpBYjDDu0cdCApxElH4uJAA4OveZGkbJ7X361tV5PCWCTnh8IUqIpVwb6ql.sGdKPEa1fPYADi4D.u.QpEwCQ66RbmPkMrVhim5XKdwhV59TfeEE.C198HCEY; reacted_chap_566264=1; reacted_chap_566263=1; reacted_chap_566262=1; reacted_chap_566261=1; reacted_chap_566260=1; reacted_chap_566259=1; reacted_chap_566258=1; reacted_chap_566245=1; reacted_chap_566244=1; reacted_chap_566243=1; reacted_chap_566242=1; reacted_chap_566241=1; reacted_chap_566240=1; reacted_chap_566239=1; reacted_chap_566238=1; reacted_chap_566237=1; reacted_chap_566236=1; reacted_chap_566235=1; reacted_chap_566234=1; useruid=uid-tne61o77661773499928606; reacted_chap_566225=1; reacted_chap_566224=1; reacted_chap_566223=1; reacted_chap_567455=1; reacted_chap_567454=1; reacted_chap_566188=1; reacted_chap_496942=1; reacted_chap_497702=1; reacted_chap_498489=1; reacted_chap_519240=1; reacted_chap_525076=1; reacted_chap_554914=1; reacted_chap_556675=1; reacted_chap_558434=1; reacted_chap_560270=1; reacted_chap_561954=1; reacted_chap_563225=1; reacted_chap_564777=1; reacted_chap_566137=1; reacted_chap_567443=1; reacted_chap_568923=1; reacted_chap_570126=1; reacted_chap_571329=1; reacted_chap_572540=1; reacted_chap_577764=1; reacted_chap_579308=1; reacted_chap_580575=1; reacted_chap_608173=1; reacted_chap_606984=1; reacted_chap_608171=1; reacted_chap_606983=1; reacted_chap_606982=1; reacted_chap_606981=1; reacted_chap_606980=1; reacted_chap_606979=1; reacted_chap_606978=1; reacted_chap_606977=1; reacted_chap_622714=1; reacted_chap_623723=1; reacted_chap_624796=1; reacted_chap_626137=1; reacted_chap_627365=1; reacted_chap_475341=1; reacted_chap_455767=1; reacted_chap_475340=1; reacted_chap_475339=1; reacted_chap_475338=1; reacted_chap_475335=1; reacted_chap_475334=1; reacted_chap_475333=1; reacted_chap_475332=1; reacted_chap_475331=1; reacted_chap_475330=1; reacted_chap_475329=1; reacted_chap_475328=1; reacted_chap_475327=1; reacted_chap_475326=1; reacted_chap_475325=1; reacted_chap_475324=1; reacted_chap_475323=1; reacted_chap_475322=1; reacted_chap_475321=1; reacted_chap_475320=1; reacted_chap_475319=1; reacted_chap_475318=1; reacted_chap_475317=1; reacted_chap_475316=1; reacted_chap_475315=1; reacted_chap_475314=1; reacted_chap_475313=1; reacted_chap_475303=1; reacted_chap_475302=1; reacted_chap_475301=1; reacted_chap_475300=1; reacted_chap_475299=1; reacted_chap_475298=1; reacted_chap_475297=1; reacted_chap_475296=1; reacted_chap_475295=1; reacted_chap_475294=1; reacted_chap_475293=1; reacted_chap_475292=1; reacted_chap_475291=1; reacted_chap_475290=1; reacted_chap_475289=1; reacted_chap_475288=1; reacted_chap_475287=1; reacted_chap_475286=1; reacted_chap_475285=1; reacted_chap_475284=1; reacted_chap_475283=1; reacted_chap_475282=1; reacted_chap_475281=1; demon=141472; demontemp=9d78416b941d1fac6a30; reacted_chap_475280=1; reacted_chap_475279=1; reacted_chap_475278=1; reacted_chap_475277=1; reacted_chap_475276=1; reacted_chap_475275=1; reacted_chap_475274=1; reacted_chap_475273=1; reacted_chap_475272=1; reacted_chap_475271=1; reacted_chap_475270=1; reacted_chap_475269=1; reacted_chap_475268=1; reacted_chap_475267=1; reacted_chap_471820=1; reacted_chap_455731=1; reacted_chap_471819=1; wave_filters_G3_W3=%7B%22name%22%3A%22%22%2C%22joined%22%3Atrue%2C%22unjoined%22%3Atrue%2C%22cap%22%3Afalse%7D; fx_off=0; af_panel_collapsed=0; wave_filters_G3_W8=%7B%22name%22%3A%22%22%2C%22joined%22%3Atrue%2C%22unjoined%22%3Atrue%2C%22cap%22%3Afalse%7D; wave_filters_G5_W9=%7B%22name%22%3A%22%22%2C%22joined%22%3Atrue%2C%22unjoined%22%3Atrue%2C%22cap%22%3Afalse%7D; crucible_view_mode_u141472=cube; subdemon=1; show_dead_bosses_only=0; wave_filters_G3_W5=%7B%22name%22%3A%22%22%2C%22joined%22%3Atrue%2C%22unjoined%22%3Atrue%2C%22cap%22%3Afalse%7D; reacted_chap_63417=1; reacted_chap_63416=1; reacted_chap_63415=1; reacted_chap_63414=1; reacted_chap_63413=1; hide_dead_monsters=1; _ga_S2788Y508W=GS2.1.s1775156705$o53$g0$t1775156719$j46$l0$h0; last_attack=1775160016657
+origin
+https://demonicscans.org
+priority
+u=1, i
+referer
+https://demonicscans.org/battle.php?dgmid=1120687&instance_id=5486
+sec-ch-ua
+"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"
+sec-ch-ua-arch
+"x86"
+sec-ch-ua-bitness
+"64"
+sec-ch-ua-full-version
+"146.0.7680.177"
+sec-ch-ua-full-version-list
+"Chromium";v="146.0.7680.177", "Not-A.Brand";v="24.0.0.0", "Google Chrome";v="146.0.7680.177"
+sec-ch-ua-mobile
+?0
+sec-ch-ua-model
+""
+sec-ch-ua-platform
+"Windows"
+sec-ch-ua-platform-version
+"19.0.0"
+sec-fetch-dest
+empty
+sec-fetch-mode
+cors
+sec-fetch-site
+same-origin
+user-agent
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36
